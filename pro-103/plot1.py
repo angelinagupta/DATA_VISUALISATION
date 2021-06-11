49a2e1bba494fd91b8bd0b5d4c1f77a854eb6a95
Angelina Gupta <69306923+angelinagupta@users.noreply.github.com>
2021-06-11 17:16:11 +0530

    Add files via upload

---
+++ pro-103/plot1.py
@@ -0,0 +1,7 @@
+import plotly
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("D:\code\python\pro-103\covid_data.csv")
+fig = px.area(df, x="date", y="cases",color="country",title='Corona Cases')
+plotly.offline.plot(fig)
